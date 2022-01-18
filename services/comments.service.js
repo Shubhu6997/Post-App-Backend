0d9a054e8259f5b98da58f367ad1af3f72909870
Shubhu6997 <shubhamhaval619@gmail.com>
2022-01-18 11:24:54 +0530

    Added validation to add comments

--- services/comments.service.js
+++ services/comments.service.js
@@ -6,7 +6,7 @@ const services = {
             console.log("getComments method called");
             console.log(req.params);
             const data = await db.comments.find({postId : Number(req.params.id)}).toArray();
-           // console.log(data);
+            console.log(data);
             res.send(data);
             
         } catch (error) {
@@ -18,20 +18,29 @@ const services = {
     async addComment(req, res){
         try {
             console.log("addComment method is called");
-            let result = await db.comments.find({},{id : 1, _id : 0})
-                                        .sort({id : -1}).limit(1)
-                                        .toArray();
-            
-            console.log(result[0].id);
-            let newId = result[0].id + 1;
-            console.log(req.body);
+            console.log("req.params.id :",req.params.id);
+            const {email} = await db.comments.findOne({id : Number(req.params.id)})
+            console.log("email :", email);
+            console.log("req.body.username :", req.body.username);
 
-            const data = await db.comments.insertOne({id : newId, ...req.body});
-            console.log(data);
-            res.end("Your comment added successfully")
+            if(email === req.body.username){
+                let result = await db.comments.find({},{id : 1, _id : 0})
+                .sort({id : -1}).limit(1)
+                .toArray();
 
+                console.log(result[0].id);
+                let newId = result[0].id + 1;
+                console.log(req.body);
+
+                const data = await db.comments.insertOne({id : newId, ...req.body});
+                console.log(data);
+                res.send("Your comment added successfully")
+            }
+            else{
+                res.send("You are not authorized to delete this comment");
+            }    
         } catch (error) {
-            console.log("Error while fetching comments : ",error);
+            console.log("Error while adding comment : ",error);
             res.sendStatus(500);  
         }
     },
@@ -46,10 +55,10 @@ const services = {
 
             if(email === req.body.username){
                 await db.comments.deleteOne({id : Number(req.params.id)});
-                res.send("Deleted Record Successfully");
+                res.send("Deleted comment successfully");
             }
             else{
-                res.send("You are not authorized to delete this post");
+                res.send("You are not authorized to delete this comment");
             }
             
         } catch (error) {
